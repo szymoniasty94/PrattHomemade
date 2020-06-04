2a7a0cc85f6eaa7bb7a34e0b5e5d7d9923f22d37
SimkoPromnice <szymon.aleksy@gmail.com>
2020-06-04 19:28:04 +0200

    Dodaj pliki projektów.

---
+++ Homemade.BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homemade.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Homemade.Commmon/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homemade.Commmon
+{
+    public class Class1
+    {
+    }
+}
